8f1a2c0b9727cfb967db79a667d16e776573489f
tuandang <degea9@gmail.com>
2021-02-08 17:58:47 +0700

    add buildSrc

---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,27 @@
+object Plugins {
+
+    /*
+       Project Level
+    */
+    const val GRADLE = "com.android.tools.build:gradle"
+    const val DETEKT = "io.gitlab.arturbosch.detekt"
+    const val KTLINT = "org.jlleitschuh.gradle.ktlint"
+    const val GIT_HOOKS = "plugins.git-hooks"
+
+    const val CLASSPATH_GRADLE = "com.android.tools.build:gradle:${PluginVersion.GRADLE_VERSION}"
+
+    const val CLASSPATH_MP_CHART = "com.github.dcendents:android-maven-gradle-plugin:2.1"
+
+    /*
+        Module Level
+     */
+    const val DAGGER_HILT_PLUGIN = "dagger.hilt.android.plugin"
+    const val ANDROID_APPLICATION_PLUGIN = "com.android.application"
+    const val ANDROID_DYNAMIC_FEATURE_PLUGIN = "com.android.dynamic-feature"
+    const val ANDROID_LIBRARY_PLUGIN = "com.android.library"
+
+    const val KOTLIN_ANDROID_PLUGIN = "kotlin-android"
+    const val KOTLIN_ANDROID_EXTENSIONS_PLUGIN = "kotlin-android-extensions"
+    const val KOTLIN_KAPT_PLUGIN = "kotlin-kapt"
+    const val NAVIGATION_SAFE_ARGS = "androidx.navigation.safeargs.kotlin"
+}
---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,8 @@
+object PluginVersion {
+    const val KOTLIN_VERSION = "1.4.20"
+    const val GRADLE_VERSION = "4.1.2"
+
+    const val NAV_SAFE_ARGS_VERSION = "2.3.0"
+    const val KTLINT_VERSION = "9.3.0"
+    const val DETEKT_VERSION = "1.10.0"
+}
