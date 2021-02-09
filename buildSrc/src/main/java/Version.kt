b4727733da79b41db39a25b01724d71f3085c2f7
tuandang <degea9@gmail.com>
2021-02-09 20:48:52 +0700

    add version to manage some configs

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -8,13 +8,13 @@ object PluginVersion {
 }
 
 object AndroidVersion {
-    const val MIN_SDK_VERSION = 21
+    const val MIN_SDK_VERSION = 23
     const val COMPILE_SDK_VERSION = 29
     const val TARGET_SDK_VERSION = COMPILE_SDK_VERSION
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
 
-    const val APPLICATION_ID = "com.smarttoolfactory.propertyfindar"
+    const val APPLICATION_ID = "com.degea9.android.food.foodrecipe"
 }
 
 object Version {
