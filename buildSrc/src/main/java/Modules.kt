83fed56da224ae11759de67007153e9354c2f711
tuandang <degea9@gmail.com>
2021-02-09 22:01:11 +0700

    Create home module as a feature module

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,20 @@
+object Modules {
+
+    const val APP = ":app"
+    val featureHome = ":features:home"
+    val featureDetail = ":features:detail"
+
+    object AndroidLibrary {
+        const val CORE = ":libraries:core"
+        const val DATA = ":libraries:data"
+        const val DOMAIN = ":libraries:domain"
+        const val TEST_UTILS = ":libraries:test-utils"
+    }
+
+    /**
+     * Dynamic Feature Modules
+     */
+    object DynamicFeature {
+
+    }
+}
