7c58261a6454238de9ef9121668bd47294a7e59e
tuandang <degea9@gmail.com>
2021-02-12 23:45:35 +0700

    add remote module

--- data/remote/src/main/java/com/degea9/foodrecipe/remote/response/InstructionResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.degea9.foodrecipe.remote.response
-
-data class InstructionResponse(
-
-    val name: String?,
-
-    val steps: List<StepResponse>?
-)
--- data/remote/src/main/java/com/degea9/foodrecipe/remote/response/ListRecipeResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.degea9.foodrecipe.remote.response
-
-data class ListRecipeResponse(
-
-    val results: List<RecipeResponse>
-)
