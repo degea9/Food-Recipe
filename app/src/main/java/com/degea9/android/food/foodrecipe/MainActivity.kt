19d9584d60e626d4403ce295b54e87ee794837eb
tuandang <tuandang@okxe.vn>
2021-02-26 13:59:22 +0700

    add common-android for common ui between features

--- app/src/main/java/com/degea9/android/food/foodrecipe/MainActivity.kt
+++ app/src/main/java/com/degea9/android/food/foodrecipe/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.degea9.android.food.foodrecipe
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.Window
@@ -13,12 +14,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val window: Window = window
-        val winParams: WindowManager.LayoutParams = window.getAttributes()
-        winParams.flags =
-            winParams.flags and WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS.inv()
-        window.setAttributes(winParams)
-        window.getDecorView()
-            .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        window.statusBarColor = Color.TRANSPARENT
+       // window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 }
