f1e21e5190a43ec55ae623f275e65d604e41b656
degea9 <degea9@gmail.com>
2021-02-28 21:42:04 +0700

    display instruction steps

--- features/recipe-detail/src/main/java/com/degea9/android/food/foodrecipe/recipe_detail/InstructionFragment.kt
+++ features/recipe-detail/src/main/java/com/degea9/android/food/foodrecipe/recipe_detail/InstructionFragment.kt
@@ -7,12 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import com.degea9.android.food.foodrecipe.recipe_detail.databinding.FragmentRecipeDetailBinding
+import com.degea9.android.food.foodrecipe.recipe_detail.databinding.FragmentRecipeInstructionBinding
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class RecipeInstructionFragment : Fragment() {
+
+    private lateinit var binding:FragmentRecipeInstructionBinding
+
+    private val instructionController: InstructionController = InstructionController()
 
     private val recipeDetailViewModel: RecipeDetailViewModel by viewModels(
         ownerProducer = { requireParentFragment() }
@@ -27,17 +33,24 @@ class RecipeInstructionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_instruction, container, false)
+        binding = FragmentRecipeInstructionBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setup()
         setupObserver()
+    }
+
+    private fun setup(){
+        binding.rvInstructions.adapter = instructionController.adapter
     }
 
     private fun setupObserver(){
         recipeDetailViewModel.recipeDetailLiveData?.observe(viewLifecycleOwner) {
-            Timber.d("recipe detail inside instruction $it")
+            Timber.d("recipe instruction ${it.analyzedInstructions?.getOrNull(0)}")
+            instructionController.setData(it.analyzedInstructions?.getOrNull(0))
         }
     }
 }
