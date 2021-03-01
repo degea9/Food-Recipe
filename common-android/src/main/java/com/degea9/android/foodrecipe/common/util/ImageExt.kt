be6b394afad78314d20bbaee1ae43a3c1f8cb172
thangnguyen <thangnguyen@okxe.vn>
2021-03-01 17:36:52 +0700

    load origin image

--- common-android/src/main/java/com/degea9/android/foodrecipe/common/util/ImageExt.kt
+++ common-android/src/main/java/com/degea9/android/foodrecipe/common/util/ImageExt.kt
@@ -1,15 +1,20 @@
 package com.degea9.android.foodrecipe.common.util
 
+import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 
 @BindingAdapter("rawImageSrc")
 fun setRawImageUrl(view: ImageView, path: String?) {
+    Log.d("AAAA", "URL: ${path}")
     try {
         Glide
             .with(view.context)
             .load(path)
+            .apply(RequestOptions.overrideOf(Target.SIZE_ORIGINAL))
             .into(view)
     } catch (e: Exception) {
         e.printStackTrace()
