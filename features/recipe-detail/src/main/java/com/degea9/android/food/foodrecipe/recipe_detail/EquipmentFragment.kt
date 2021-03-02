c43bc82954b6d96071f29c2ac8ad0507b98eb2c7
thangnguyen <thangnguyen@okxe.vn>
2021-03-02 11:06:08 +0700

    show equipment in recipe detail

--- features/recipe-detail/src/main/java/com/degea9/android/food/foodrecipe/recipe_detail/EquipmentFragment.kt
+++ features/recipe-detail/src/main/java/com/degea9/android/food/foodrecipe/recipe_detail/EquipmentFragment.kt
@@ -4,23 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import com.degea9.android.food.foodrecipe.recipe_detail.databinding.FragmentEquipmentBinding
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class EquipmentFragment : Fragment() {
 
+    private lateinit var binding: FragmentEquipmentBinding
+    private val equipmentController = EquipmentController()
+    private val recipeDetailViewModel: RecipeDetailViewModel by viewModels(
+        ownerProducer = {requireParentFragment()}
+    )
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_equipment, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_equipment, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+        setupObserver()
+    }
+    private fun setup(){
+        val layoutManager = GridLayoutManager(requireContext(), SPAN_COUNT)
+        equipmentController.spanCount = SPAN_COUNT
+        layoutManager.spanSizeLookup = equipmentController.spanSizeLookup
+        binding.rvEquipment.layoutManager = layoutManager
+        binding.rvEquipment.adapter = equipmentController.adapter
+    }
+
+    private fun setupObserver(){
+        recipeDetailViewModel.recipeDetailLiveData?.observe(viewLifecycleOwner){
+            Timber.d("recipe equipment ${it.analyzedInstructions?.getOrNull(0)}")
+            equipmentController.setData(it.analyzedInstructions?.getOrNull(0))
+        }
+    }
+
+    companion object {
+        private const val SPAN_COUNT = 3
     }
 
 }
