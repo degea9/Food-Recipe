9de56d713bb9122da294ef6eda0355a89f974945
thangnguyen <thangnguyen@okxe.vn>
2021-03-10 14:12:49 +0700

    create add history usecase

--- domain/src/main/java/com/degea9/android/foodrecipe/domain/search/InsertHistoryRecipesUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.degea9.android.foodrecipe.domain.search
-
-import com.degea9.android.foodrecipe.domain.repository.RecipeRepository
-import javax.inject.Inject
-
-class InsertHistoryRecipesUseCase @Inject constructor(private val recipeRepository: RecipeRepository) {
-    private fun saveRecipeToLocal(recipe: RecipeRepository){
-    }
-}
