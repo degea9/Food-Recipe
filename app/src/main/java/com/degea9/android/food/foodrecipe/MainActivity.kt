4f515b479bb4f2628531e79a1f084cc8c587e1ee
thangnguyen <thangnguyen@okxe.vn>
2021-03-10 16:06:39 +0700

    auto hide keyboard when navigate

--- app/src/main/java/com/degea9/android/food/foodrecipe/MainActivity.kt
+++ app/src/main/java/com/degea9/android/food/foodrecipe/MainActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.degea9.android.foodrecipe.core.setupWithNavController1
@@ -15,7 +19,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +29,11 @@ class MainActivity : AppCompatActivity() {
         window.statusBarColor = Color.TRANSPARENT
        // window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
         setupBottomNavigationBar()
+
+        val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        navController.addOnDestinationChangedListener(this)
     }
 
     /**
@@ -41,7 +50,19 @@ class MainActivity : AppCompatActivity() {
             containerId = R.id.nav_host,
             intent = intent
         )
+    }
 
+    override fun onDestinationChanged(controller: NavController, destination: NavDestination, arguments: Bundle?) {
+        currentFocus?.hideKeyboard()
+    }
+    fun View.hideKeyboard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        findNavController(R.id.nav_host).removeOnDestinationChangedListener(this)
 
     }
 
